86310486819bb6b05bb6208504156f6f98c9d124
Jonny Krysh <jonnykry93@gmail.com>
2013-09-21 19:00:24 -0500

    Android for Image Search
    Still unfinished, but the Camera button works, doesn't store image yet.

---
+++ src/com/example/imagesearch/CameraActivity.java
@@ -0,0 +1,37 @@
+package com.example.imagesearch;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Camera;
+import android.view.Menu;
+import android.widget.FrameLayout;
+
+public class CameraActivity extends Activity {
+
+	// Still working on this.
+	
+    private Camera mCamera;
+    private CameraPreview mPreview;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camera);
+		
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.camera, menu);
+		return true;
+	}
+
+}
---
+++ src/com/example/imagesearch/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.imagesearch;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.service.textservice.SpellCheckerService.Session;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	private Uri imageUri;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		final Button camera = (Button) findViewById(R.id.button1);
+		final Button gallery = (Button) findViewById(R.id.button2);
+		
+		camera.setOnClickListener(cameraListener);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+			if(resultCode == RESULT_OK) {
+				// TO-DO networking with server
+				Intent temp = new Intent(MediaStore.ACTION_IMAGE_CAPTURE, data.getData());
+				startActivity(temp);
+			}
+		}
+		
+		
+	}
+
+	// Camera Button Click
+	private OnClickListener cameraListener = new OnClickListener() {
+		public void onClick(View v) {
+			String fileName = "image-search.jpg";
+			ContentValues values = new ContentValues();
+			values.put(MediaStore.Images.Media.TITLE, fileName);
+			values.put(MediaStore.Images.Media.DESCRIPTION,"Image capture by camera");
+			imageUri = getContentResolver().insert(
+			        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+			intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+			startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	    }
+	};
+	
+	// Gallery Button Click
+	private OnClickListener galleryListener = new OnClickListener() {
+		public void onClick(View v) {
+			// TO-DO
+	    }
+	};
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+}
